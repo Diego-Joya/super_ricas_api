96ddadde1daeb1c8fa513b3aae7db165d7149ca4
yekogarcia <yekogarcia@yahoo.com>
2022-11-23 20:57:53 -0500

    config http

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-const express = require('express');
-require('dotenv').config();
-const routerApi = require('./routes')
-const {logErrors, errorHandler, boomErrorHandler} = require('./middlewares/error_handler');
+const express = require("express");
+require("dotenv").config();
+const routerApi = require("./routes");
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+} = require("./middlewares/error_handler");
 
-const cors = require('cors');
-const{checkApiKey}= require('./middlewares/auth_handler');
+const cors = require("cors");
+const { checkApiKey } = require("./middlewares/auth_handler");
+const http = require("http");
 
 const app = express();
 app.use(cors());
-require('./utils/auth');
+require("./utils/auth");
 
 // app.get('/nueva-ruta', checkApiKey,(req, res)=>{
-app.get('/nueva-ruta', checkApiKey,(req, res)=>{
-    res.send('holaaa nene');
+app.get("/nueva-ruta", checkApiKey, (req, res) => {
+  res.send("holaaa nene");
 });
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use(express.json());
 
@@ -27,6 +32,8 @@ app.use(errorHandler);
 
 console.log(process.env.PORT);
 
-app.listen(process.env.PORT, () =>{
-    console.log(`Sevidor corriendo ${process.env.PORT}`);
+http.createServer(app).listen(process.env.PORT);
+
+app.listen(() => {
+  console.log(`Sevidor corriendo ${process.env.PORT}`);
 });
