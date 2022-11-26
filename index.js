714dc9c4639d2ed7b87b9502562580967a9d8a1c
yekogarcia <yekogarcia@yahoo.com>
2022-11-26 15:31:57 -0500

    con server

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(errorHandler);
 console.log(process.env.PORT);
 
 // https.createServer(app).listen(process.env.PORT, process.env.HOST);
-// http.createServer(app).listen(process.env.PORT, process.env.HOST);
+http.createServer(app).listen(process.env.PORT, process.env.HOST);
 
 app.listen(() => {
   console.log(`Sevidor corriendo ${process.env.PORT}`);
